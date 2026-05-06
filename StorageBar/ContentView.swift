a2b7d45ff530bdeaa69aece57f82d53f29ee9128
1Verona <jgabrielwverona@gmail.com>
2026-05-06 20:34:58 -0300

    Initial Commit

---
+++ StorageBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StorageBar
+//
+//  Created by João Gabriel Weber dos Santos Verona on 06/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StorageBar/StorageBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageBarApp.swift
+//  StorageBar
+//
+//  Created by João Gabriel Weber dos Santos Verona on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct StorageBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
